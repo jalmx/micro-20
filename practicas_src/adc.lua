f21330a24fb54c1f890e60eeac6e1d96e282348f
Josef Leyva <jalm_x@hotmail.com>
2020-04-10 21:16:29 -0500

    book

--- practicas_src/adc.lua
+++
@@ -1,9 +0,0 @@
--- https://nodemcu.readthedocs.io/en/master/modules/adc/
-
-adc.force_init_mode(adc.INIT_ADC) -- por buenas prácticas se inicia el adc
-
-while true do
-    local an = adc.read(0)
-    print("valor del adc ".. an)
-    tmr.delay(100000)
-end
